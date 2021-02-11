bbc1515a0c3a3bbdfd47e28b56669d897ad49d08
MukeshChaudhary <mukesh.chaudhary@revature.net>
2021-02-11 10:51:48 -0600

    saved yelp

--- hackathon/src/app/app.module.ts
+++ hackathon/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { YelpComponent } from './component/yelp/yelp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    YelpComponent
   ],
   imports: [
     BrowserModule,
---
+++ hackathon/src/app/component/yelp/yelp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-yelp',
+  templateUrl: './yelp.component.html',
+  styleUrls: ['./yelp.component.css']
+})
+export class YelpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
